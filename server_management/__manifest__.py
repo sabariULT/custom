06ca70581af9bbfb5cb4a9308b6f37a4adfc4dfc
KG Sabari <sabari.krishnagiri@ultsglobal.com>
2024-10-25 18:57:06 +0530

    [ADD]: server_management

---
+++ server_management/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Server Management',
+    'version': '12.0',
+    'category': 'Employees',
+    'description': """
+Managing Company's Servers:
+* Server Management Menu: Human Resources >> Server Management >> Server Management
+* Server Management Groups configurable from user profile: Server Access: Limited and Server Access: Full
+    """,
+    'author': 'Rightechs Solutions',
+    'website': 'rightechs.net',
+    'depends': ['base', 'hr'],
+    'data': [
+        'security/ir_security.xml',
+        'security/ir.model.access.csv',
+        'views/server_view.xml',
+        'views/database_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
